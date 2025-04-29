0d2277d65449d8fad67670dbaf090b28e949b2f3
unknown <xrosina@ad.fi.muni.cz>
2025-04-29 18:57:43 +0200

    fix

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -2,6 +2,8 @@ import sys
 import os
 import yaml
 import flask
+import importlib
+
 
 app = flask.Flask(__name__)
 
@@ -25,7 +27,7 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    urllib = importlib.import_module(f"urllib.{urllib_version}")
     # Fetch and print the requested URL
 
     try:
